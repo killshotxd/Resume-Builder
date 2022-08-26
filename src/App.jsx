8b1d14dd5ed8efa64c990532725ce10bdebe368b
killshotxd <hassanansari211@gmail.com>
2022-08-26 17:36:11 +0530

    removed Firebase login

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,11 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import React from "react";
+import Body from "./components/Body/Body";
 import Header from "./components/Header/Header";
-import Auth from "./components/Auth/Auth";
-import Home from "./Home";
-import { auth } from "./Firebase";
 const App = () => {
-  const [isAuth, setIsAuth] = useState(false);
-
-  useEffect(() => {
-    const listener = auth.onAuthStateChanged((user) => {
-      if (!user) {
-        setIsAuth(false);
-        return;
-      }
-
-      setIsAuth(true);
-    });
-
-    return () => listener();
-  }, []);
-
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          {!isAuth && (
-            <>
-              <Route path="/login" element={<Auth />} />
-              <Route path="/signUp" element={<Auth signUp />} />
-            </>
-          )}
-          <Route path="/" element={<Home auth={isAuth} />} />
-        </Routes>
-      </BrowserRouter>
+      <Header />
+      <Body />
     </div>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,8 @@
 import React from "react";
 import styles from "./Header.module.css";
 import resSvg from "../../assets/undraw.svg";
-import { signOut } from "firebase/auth";
-import { auth } from "../../Firebase";
-import { async } from "@firebase/util";
+
 const Header = (props) => {
-  const isAuth = props.auth ? true : false;
-
-  const handleLogout = async () => {
-    await signOut(auth);
-  };
-
   return (
     <div className={styles.container}>
       <div className={styles.left}>
@@ -20,14 +12,6 @@ const Header = (props) => {
         <p className={styles.heading}>
           Make your own Resume. <span>It's free</span>
         </p>
-
-        <div className={styles.btnDiv}>
-          {isAuth ? (
-            <button onClick={handleLogout}>Logout</button>
-          ) : (
-            <button onClick={handleSignIn}>SignUp</button>
-          )}
-        </div>
       </div>
       <div className={styles.right}>
         <img src={resSvg} alt="Resume" />
