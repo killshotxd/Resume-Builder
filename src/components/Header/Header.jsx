0712575fc1b44a7a12ee6e082cce21f23476c121
killshotxd <hassanansari211@gmail.com>
2022-08-26 09:43:34 +0530

    header completed

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Header.module.css";
+import resSvg from "../../assets/undraw.svg";
+const Header = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <p className={styles.heading}>
+          A <span>Resume</span> that stands out!
+        </p>
+        <p className={styles.heading}>
+          Make your own Resume. <span>It's free</span>
+        </p>
+      </div>
+      <div className={styles.right}>
+        <img src={resSvg} alt="Resume" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
